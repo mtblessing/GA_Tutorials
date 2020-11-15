8d78e7f47c99758d3c4306dca7495dcb9ca87819
Tongai <tbm@adatis.co.uk>
2020-11-15 21:18:00 +0000

    Commented lines in auto app.

--- auto.py
+++ auto.py
@@ -11,16 +11,16 @@
 # print(a)
 
 name1 = "TB"
-height_m1 = 2
-weight_kg1 = 90
+height_m1 = 1.75
+weight_kg1 = 65
 
-name2 = "TB Sister"
-height_m2 = 1.8
-weight_kg2 = 70
+# name2 = "TB Sister"
+# height_m2 = 1.8
+# weight_kg2 = 70
 
-name3 = "TB Brother"
-height_m3 = 2.5
-weight_kg3 = 160
+# name3 = "TB Brother"
+# height_m3 = 2.5
+# weight_kg3 = 160
 
 def bmi_calc(name, height_m, weight_kg):
     bmi = weight_kg / (height_m ** 2)
@@ -32,9 +32,9 @@ def bmi_calc(name, height_m, weight_kg):
         return name + " Is overweight"
 
 result1 = bmi_calc(name1, height_m1, weight_kg1)
-result2 = bmi_calc(name2, height_m2, weight_kg2)
-result3 = bmi_calc(name3, height_m3, weight_kg3)
+# result2 = bmi_calc(name2, height_m2, weight_kg2)
+# result3 = bmi_calc(name3, height_m3, weight_kg3)
 
 print(result1)
-print(result2)
-print(result3)
+# print(result2)
+# print(result3)
