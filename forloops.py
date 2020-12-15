f1deb8627e8845aa3ee586f3a3e90015ed3d23fd
Tongai <tbm@adatis.co.uk>
2020-12-15 00:01:29 +0000

    Working out range

--- forloops.py
+++ forloops.py
@@ -1,17 +1,17 @@
-#cars = ["BMW", "Golf","Mazda","Polo"]
+cars = ['BMW',"Golf","Mazda","Polo"]
 #print(cars[0::2])
-# numbers = [1,"BMW",3,"Honda",5]
+# cars = ['Nissan','BMW','Landrover','Honda']
 # for cars in range(1):
-#     print(numbers[0])
+#     print(cars[0])
 # for cars in range(2):
-#     print(numbers[1])
+#     print(cars[1])
 # for cars in range(3):
-#     print(numbers[2])
+#     print(cars[2])
 # for cars in range(4):
-#     print(numbers[3])
+#     print(cars[3])
 # for cars in range(2):
-#     print(numbers[4])
+#     print(cars[4])
 
-#for i in range(10):
-   # print("Car")
+for i in range(5):
+    print(cars[0])
 
