690db5ed0c164fc98b963cc8c05c9ed202ea2afa
Tongai <tongai.blessing@gmail.com>
2024-06-27 23:27:54 +0100

    Created a new file called Country

---
+++ Country.py
@@ -0,0 +1,6 @@
+Country = str(input("What Country "))
+#print("Your Country is,", Country)
+if Country == "UK":
+    print(" The Capital city is of",Country,"London")
+else:
+    print("country not found")
